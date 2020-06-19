be1145e239fadf804e7ced9fa67ae383872b18ca
ratovia <ratovia@hotmail.co.jp>
2020-06-20 03:03:54 +0900

    update

---
+++ src/components/MainView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="wrapper">
+    <img id="pointer-image" src="../assets/pointer.png" alt="pointer-image">
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  #wrapper {
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+    
+  }
+
+  #pointer-image {
+    height: 100%;
+    width: 100%;
+    -webkit-app-region: drag
+  }
+</style>
